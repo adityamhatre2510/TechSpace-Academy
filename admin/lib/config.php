122c7dbb84486a8d9eea2a5674c6272cb105e8c5
Mybvc365 <t.kotwal365@mybvc.ca>
2024-12-11 13:57:41 -0700

    Update config.php

--- admin/lib/config.php
+++ admin/lib/config.php
@@ -5,9 +5,9 @@ define("BASE_URL", dirname(__DIR__));
 define("ROOT_PATH", "/admin");
 define("CURRENCY", "CAD");
 
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_HOST", "group2-techspace-academy-rds.czptxhzjxjrt.us-east-1.rds.amazonaws.com");
+define("DB_USERNAME", "admin");
+define("DB_PASSWORD", "techspaceacademy2");
 define("DB_NAME", "techspace_academy");
 
 function dd($data=null, $e=0) {
